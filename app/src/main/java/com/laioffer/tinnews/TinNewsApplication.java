fef110dc374116cd90ff313df491793282536e2d
seanking <152>
2023-03-30 15:26:34 +0800

    first commit

---
+++ app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -0,0 +1,27 @@
+package com.laioffer.tinnews;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.room.Room;
+
+import com.laioffer.tinnews.database.TinNewsDatabase;
+import com.laioffer.tinnews.model.Article;
+
+import java.util.UUID;
+
+public class TinNewsApplication extends Application {
+
+    private static TinNewsDatabase database;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        database = Room.databaseBuilder(this, TinNewsDatabase.class, "tinnews_db").build();
+    }
+
+    public static TinNewsDatabase getDatabase() {
+        return database;
+    }
+
+}
